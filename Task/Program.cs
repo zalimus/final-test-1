c9f871b79b506b04c01643fdf9dbcce8be4e7ede
zalimus <zalim011@gmail.com>
2023-02-09 22:15:17 +0300

    Добавление метода Print1DArr

--- Task/Program.cs
+++ Task/Program.cs
@@ -34,3 +34,13 @@ string[] FilteredArray(string[] originalArray)
 
     return filteredArray;
 }
+
+void Print1DArr(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
